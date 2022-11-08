412a696fa8cd17600de7125ad597d27d2989deef
mevitechnologies <mevitechnologies@gmail.com>
2022-11-08 18:01:06 +0530

    cm
    cm

--- mevi/settings.py
+++ mevi/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'mevi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
