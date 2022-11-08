70472d8f13551cd61d8bda20cbf4a39b8c45aa9e
mevitechnologies <mevitechnologies@gmail.com>
2022-11-08 17:50:10 +0530

    com
    com

--- mevi/wsgi.py
+++ mevi/wsgi.py
@@ -12,6 +12,3 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mevi.settings')
-
-application = get_wsgi_application()
-app = application
